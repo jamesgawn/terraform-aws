25277e64538358d08cd21676310b358c82e231e6
James Gawn <james@gawn.uk>
2021-08-29 12:54:47 +0100

    Move gawn.uk to icloud

--- gsuite.tf
+++ gsuite.tf
@@ -1,37 +1,3 @@
-// gawn.uk
-data "aws_route53_zone" "gawn-uk" {
-  name = "gawn.uk"
-}
-resource "aws_route53_record" "gawn-uk-email-mx" {
-  zone_id = data.aws_route53_zone.gawn-uk.zone_id
-  name = data.aws_route53_zone.gawn-uk.name
-  type = "MX"
-  ttl = "300"
-  records = [    
-    "30 aspmx5.googlemail.com",
-    "30 aspmx2.googlemail.com",
-    "30 aspmx3.googlemail.com",
-    "30 aspmx4.googlemail.com",
-    "20 alt1.aspmx.l.google.com",
-    "10 aspmx.l.google.com",
-    "20 alt2.aspmx.l.google.com",
-  ]
-}
-resource "aws_route53_record" "gawn-uk-email-txt-domainkey" {
-  zone_id = data.aws_route53_zone.gawn-uk.zone_id
-  name = data.aws_route53_zone.gawn-uk.name
-  type = "TXT"
-  ttl = "300"
-  records = ["v=spf1 include:_spf.google.com ~all"]
-}
-resource "aws_route53_record" "gawn-uk-email-dkim" {
-  zone_id = data.aws_route53_zone.gawn-uk.zone_id
-  name = "mailauth._domainkey.${data.aws_route53_zone.gawn-uk.name}"
-  type = "TXT"
-  ttl = "300"
-  records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCSKIualHjOyfzkR6VWrMTx3vPzZhwzUcu0VMZFRYxPNh8X2l0dWeZJdCn5W+wLaVFK6PnYerrDW6b0lGzVPE/QDlgAGCG8MkqyLf85PTqHxXtEba4ykdqJZAL9fueiTI1Imf0KMiY13yro+IaVB8E6LL51wUQfOlpBbbA/K/Pb8wIDAQAB"]
-}
-
 // gawn.co.uk
 data "aws_route53_zone" "gawn-co-uk" {
   name = "gawn.co.uk"
--- icloud.tf
+++ icloud.tf
@@ -78,3 +78,29 @@ resource "aws_route53_record" "sb-co-uk-email-dkim" {
   ttl = "600"
   records = ["sig1.dkim.scary-biscuits.co.uk.at.icloudmailadmin.com."]
 }
+
+// gawn.uk
+data "aws_route53_zone" "gawn-uk" {
+  name = "gawn.uk"
+}
+resource "aws_route53_record" "gawn-uk-email-mx" {
+  zone_id = data.aws_route53_zone.gawn-uk.zone_id
+  name = data.aws_route53_zone.gawn-uk.name
+  type = "MX"
+  ttl = "300"
+  records = ["1 mx01.mail.icloud.com", "2 mx02.mail.icloud.com"]
+}
+resource "aws_route53_record" "gawn-uk-email-txt-domainkey" {
+  zone_id = data.aws_route53_zone.gawn-uk.zone_id
+  name = data.aws_route53_zone.gawn-uk.name
+  type = "TXT"
+  ttl = "300"
+  records = ["apple-domain=ikpQVrVN6M7Ktyv3", "v=spf1 redirect=icloud.com"]
+}
+resource "aws_route53_record" "gawn-uk-email-dkim" {
+  zone_id = data.aws_route53_zone.gawn-uk.zone_id
+  name = "sig1._domainkey.${data.aws_route53_zone.gawn-uk.name}"
+  type = "CNAME"
+  ttl = "300"
+  records = ["sig1.dkim.gawn.uk.at.icloudmailadmin.com."]
+  }
