00afcb60c1ea59841be6e40279359f8554deb613
James Gawn <james@gawn.uk>
2022-06-12 15:37:18 +0000

    Update minimum TLS version for cf

--- modules/gateway-serverless/custom-domain-with-redirect/main.tf
+++ modules/gateway-serverless/custom-domain-with-redirect/main.tf
@@ -134,6 +134,7 @@ resource "aws_cloudfront_distribution" "distribution" {
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
