a37f790dcd9be9a9427e057ecf4a3b8e6bf99e87
James Gawn <james@gawn.uk>
2022-06-12 13:20:42 +0000

    Add in logic to validate cert using dns

--- modules/gateway-serverless/custom-domain-with-redirect/main.tf
+++ modules/gateway-serverless/custom-domain-with-redirect/main.tf
@@ -13,7 +13,7 @@ variable "zone" {
 }
 
 variable "domains" {
-  type = list(object({zone=string, domain=string}))
+  type = list(object({zone_id=string, domain=string}))
   description = "The list of domains to map to the gateway"
 }
 
@@ -53,29 +53,29 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-# resource "aws_route53_record" "cert_validation" {
-#   provider = aws.default
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     } 
-#   }
+resource "aws_route53_record" "cert_validation" {
+  provider = aws.default
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    } 
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.zone.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.domains[index(var.domains.*.domain, dvo.domain_name)].zone
+}
 
-# resource "aws_acm_certificate_validation" "cert" {
-#   provider = aws.us-east-1
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "cert" {
+  provider = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
 
 # /*
 #  * Cloudfront distribution
