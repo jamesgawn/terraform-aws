a1e24ff4cdc91ae188a936709eb721fd29bf5d46
James Gawn <james@gawn.uk>
2022-06-12 12:23:11 +0000

    Use SNI for certificates

--- modules/gateway-serverless/custom-domain-with-redirect/main.tf
+++ modules/gateway-serverless/custom-domain-with-redirect/main.tf
@@ -113,6 +113,7 @@ resource "aws_cloudfront_distribution" "distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+    ssl_support_method = "sni-only"
   }
 }
 
