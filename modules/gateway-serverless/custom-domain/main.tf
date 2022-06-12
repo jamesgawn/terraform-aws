bda2fb0759cfa5ce991abf2d76e7d733004d09fe
James Gawn <james@gawn.uk>
2022-06-12 07:17:44 +0000

    Added logic to automatically create certificate

--- modules/gateway-serverless/custom-domain/main.tf
+++ modules/gateway-serverless/custom-domain/main.tf
@@ -1,8 +1,3 @@
-variable "cert_domain" {
-  type = string
-  description = "The primary domain name on the cert within certificate manager. e.g. gawn.uk"
-}
-
 variable "domain" {
   type = string
   description = "The domain you wish to attach to the gateway. e.g. api.gawn.uk"
@@ -32,7 +27,7 @@ resource "aws_apigatewayv2_domain_name" "api" {
   domain_name = var.domain
 
   domain_name_configuration {
-    certificate_arn = data.aws_acm_certificate.api_cert.arn
+    certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
     endpoint_type   = "REGIONAL"
     security_policy = "TLS_1_2"
   }
@@ -59,4 +54,35 @@ resource "aws_route53_record" "api" {
     zone_id                = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert-validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.cert.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
