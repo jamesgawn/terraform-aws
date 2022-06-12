71a7975209c160b206da824dc368354a5fe9cb64
James Gawn <james@gawn.uk>
2022-06-12 13:09:46 +0000

    Breakdown starting with certificate

--- modules/gateway-serverless/custom-domain-with-redirect/main.tf
+++ modules/gateway-serverless/custom-domain-with-redirect/main.tf
@@ -13,7 +13,7 @@ variable "zone" {
 }
 
 variable "domains" {
-  type = list(string)
+  type = list(object({zone=string, domain=string}))
   description = "The list of domains to map to the gateway"
 }
 
@@ -43,131 +43,131 @@ data "aws_apigatewayv2_api" "gateway" {
  */
 resource "aws_acm_certificate" "cert" {
   provider = aws.us-east-1
-  domain_name       = var.domains[0]
+  domain_name       = var.domains[0].domain
   validation_method = "DNS"
 
-  subject_alternative_names = var.domains
+  subject_alternative_names = var.domains[*].domain
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  provider = aws.default
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "cert_validation" {
+#   provider = aws.default
+#   for_each = {
+#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     } 
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.zone.zone_id
+# }
 
-resource "aws_acm_certificate_validation" "cert" {
-  provider = aws.us-east-1
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert" {
+#   provider = aws.us-east-1
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
 
-/*
- * Cloudfront distribution
- */
+# /*
+#  * Cloudfront distribution
+#  */
 
-resource "aws_cloudfront_distribution" "distribution" {
-  provider = aws.default
-  origin {
-    origin_id = "gateway"
-    domain_name = substr(data.aws_apigatewayv2_api.gateway.api_endpoint, 8, 1000)
+# resource "aws_cloudfront_distribution" "distribution" {
+#   provider = aws.default
+#   origin {
+#     origin_id = "gateway"
+#     domain_name = substr(data.aws_apigatewayv2_api.gateway.api_endpoint, 8, 1000)
 
-    custom_origin_config {
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols = ["TLSv1.2"]
-      https_port = 443
-      http_port = 80
-    }
+#     custom_origin_config {
+#       origin_protocol_policy = "https-only"
+#       origin_ssl_protocols = ["TLSv1.2"]
+#       https_port = 443
+#       http_port = 80
+#     }
 
-    origin_path = "/prod"
-  }
+#     origin_path = "/prod"
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
+#   enabled             = true
+#   is_ipv6_enabled     = true
 
-  aliases = var.domains
+#   aliases = var.domains
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "gateway"
+#   default_cache_behavior {
+#     allowed_methods  = ["GET", "HEAD"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "gateway"
 
-    forwarded_values {
-      query_string = true
+#     forwarded_values {
+#       query_string = true
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    compress = true
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
+#     compress = true
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#   }
 
-  price_class = "PriceClass_100"
+#   price_class = "PriceClass_100"
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
-    ssl_support_method = "sni-only"
-  }
-}
+#   viewer_certificate {
+#     acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+#     ssl_support_method = "sni-only"
+#   }
+# }
 
-/*
- * Map domains to cloudfront distribution
- */
+# /*
+#  * Map domains to cloudfront distribution
+#  */
 
-resource "aws_route53_record" "api-ipv4" {
-  provider = aws.default
-  for_each = toset(var.domains)
+# resource "aws_route53_record" "api-ipv4" {
+#   provider = aws.default
+#   for_each = toset(var.domains)
 
-  name    = each.key
-  type    = "A"
-  zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = each.key
+#   type    = "A"
+#   zone_id = data.aws_route53_zone.zone.zone_id
 
-  alias {
-    name                   = aws_cloudfront_distribution.distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-  allow_overwrite = true
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.distribution.domain_name
+#     zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "api-ipv6" {
-  provider = aws.default
-  for_each = toset(var.domains)
+# resource "aws_route53_record" "api-ipv6" {
+#   provider = aws.default
+#   for_each = toset(var.domains)
 
-  name    = each.key
-  type    = "AAAA"
-  zone_id = data.aws_route53_zone.zone.zone_id
+#   name    = each.key
+#   type    = "AAAA"
+#   zone_id = data.aws_route53_zone.zone.zone_id
 
-  alias {
-    name                   = aws_cloudfront_distribution.distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-  allow_overwrite = true
-}
+#   alias {
+#     name                   = aws_cloudfront_distribution.distribution.domain_name
+#     zone_id                = aws_cloudfront_distribution.distribution.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+#   allow_overwrite = true
+# }
